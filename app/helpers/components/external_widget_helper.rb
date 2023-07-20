f26b7e586caa13e64f3cf30d774d4cfb21cd3b14
Kevin Lee Drum <kevin@kevindrum.com>
2023-07-20 11:50:14 -0400

    Use external_preview_url and/or demo values OT-201

--- app/helpers/components/external_widget_helper.rb
+++ app/helpers/components/external_widget_helper.rb
@@ -1,7 +1,12 @@
 module Components::ExternalWidgetHelper
-  def populate_url_variables(url)
+  def populate_url_variables(url, demo = false)
     return url unless url.include?("{")
-    url_variables.each do |key, value|
+    variables = if demo
+      demo_values
+    else
+      Rails.env.test? ? test_values : session_values
+    end
+    variables.each do |key, value|
       url.gsub!(Regexp.new("{#{key}}", Regexp::IGNORECASE), URI.encode_www_form_component(value.to_s))
     end
     url
@@ -9,11 +14,27 @@ module Components::ExternalWidgetHelper
 
   private
 
-  def url_variables
+  def session_values
     {
       mls_number: session.dig(:current_user, :mls_number),
       nrds_number: session.dig(:current_user, :nrds_number),
       full_name: session.dig(:current_user, :full_name)
+    }
+  end
+
+  def demo_values
+    {
+      mls_number: "123456",
+      nrds_number: "123456789",
+      full_name: "Jane Doe"
+    }
+  end
+
+  def test_values
+    {
+      mls_number: "111111",
+      nrds_number: "111111111",
+      full_name: "Test User"
     }
   end
 end
